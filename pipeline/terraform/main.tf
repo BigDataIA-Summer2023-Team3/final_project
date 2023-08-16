0814870cb3c6cde75c03dcc7111e8e216782c61c
Sukruth_Mothakapally <107236457+Sukruthmothakapally@users.noreply.github.com>
2023-08-16 09:13:59 -0700

    Update main.tf

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,8 +1,8 @@
 #google cloud provider
 provider "google" {
-  credentials = file("C:\\Users\\Dell\\OneDrive - Northeastern University\\courses\\big data and intl analytics\\DAMG7245-Summer2023\\final_project\\airflow\\dags\\servicekey.json")
-  project     = "stackai-394819"
-  region      = "us-west2"
+  credentials = file("path\to\servicekey.json")
+  project     = "your project id"
+  region      = "region"
 }
 #bigquery dataset
 resource "google_bigquery_dataset" "stackai" {
@@ -130,15 +130,15 @@ resource "google_sql_database_instance" "postgres_instance" {
 
 #cloud sql database
 resource "google_sql_database" "postgres_db" {
-  name     = "stackai"
+  name     = "database name"
   instance = google_sql_database_instance.postgres_instance.name
 }
 
 #cloud sql user
 resource "google_sql_user" "postgres_user" {
-  name     = "stackai"
+  name     = "user name"
   instance = google_sql_database_instance.postgres_instance.name
-  password = "hello123"
+  password = "set your password here"
 }
 #cloud sql firewall
 output "instance_address" {
@@ -165,57 +165,57 @@ resource "google_compute_instance" "default" {
   }
 
   service_account {
-    email = "stackaiuser@stackai-394819.iam.gserviceaccount.com"
+    email = "service account email here"
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
-  #  metadata = {
-  #   startup-script = <<-EOT
-  #     #!/bin/bash
-  #     exec > >(tee -i /var/log/myscript.log)
-  #     exec 2>&1
-  #     set -eux
+   metadata = {
+   startup-script = <<-EOT
+      #!/bin/bash
+      exec > >(tee -i /var/log/myscript.log)
+      exec 2>&1
+      set -eux
 
-  #     # Remove unwanted packages
-  #     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
-  #       sudo apt-get remove -y $pkg; 
-  #     done
+    # Remove unwanted packages
+     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
+       sudo apt-get remove -y $pkg; 
+    done
 
-  #     # Install required packages
-  #     sudo apt-get update
-  #     sudo apt-get install -y ca-certificates curl gnupg software-properties-common
+    # Install required packages
+    sudo apt-get update
+   sudo apt-get install -y ca-certificates curl gnupg software-properties-common
 
-  #     # Set up Docker repository
-  #     sudo install -m 0755 -d /etc/apt/keyrings
-  #     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  #     sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  #     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  #     sudo apt-get update
-  #     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose
+    # Set up Docker repository
+     sudo install -m 0755 -d /etc/apt/keyrings
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+      sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose
 
-  #     # Allow necessary ports
-  #     sudo ufw allow 30004/tcp
-  #     sudo ufw allow 30005/tcp
-  #     sudo ufw allow 8080/tcp
+    # Allow necessary ports
+     sudo ufw allow 30004/tcp
+     sudo ufw allow 30005/tcp
+    sudo ufw allow 8080/tcp
 
-  #     # Create application directory and set ownership
-  #     sudo mkdir /app
-  #     sudo chown $(whoami):$(whoami) /app
+    # Create application directory and set ownership
+   sudo mkdir /app
+     sudo chown $(whoami):$(whoami) /app
 
-  #     # Clone your app from a GitHub repository
-  #     cd /app
-  #     git clone --branch Sukruth-branch https://github.com/Sukruthmothakapally/DAMG7245-Summer2023.git
-  #     cd DAMG7245-Summer2023/final_project
-  #     # Create .env file with specified environment variables
-  #     cat > .env <<EOF
-  #     AIRFLOW_UID=1000=2
-  #     AIRFLOW_PROJ_DIR=./airflow 
-  #     EOF
-  #     # Set the DB_HOST environment variable using the value of the instance_address output from Terraform
-  #     export DB_HOST=${google_sql_database_instance.postgres_instance.public_ip_address}
-  #     sudo docker compose up -d
-  #   EOT
-  # }
+   # Clone your app from a GitHub repository
+    cd /app
+    git clone --branch Sukruth-branch https://github.com/Sukruthmothakapally/DAMG7245-Summer2023.git
+   cd DAMG7245-Summer2023/final_project
+     # Create .env file with specified environment variables
+     cat > .env <<EOF
+      AIRFLOW_UID=1000=2
+     AIRFLOW_PROJ_DIR=./airflow 
+    EOF
+    # Set the DB_HOST environment variable using the value of the instance_address output from Terraform
+   export DB_HOST=${google_sql_database_instance.postgres_instance.public_ip_address}
+   sudo docker compose up -d
+  EOT
+  }
 
   tags = ["http-server", "https-server"]
 }
